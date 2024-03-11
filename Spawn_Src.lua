3bf03ba1300dbd2c603000782171271a2f3ba1cc
oxiclee <126022978+oxiclee@users.noreply.github.com>
2024-03-11 14:57:33 -0400

    Update Spawn_Src.lua

--- Spawn_Src.lua
+++ Spawn_Src.lua
@@ -1,53 +1,28 @@
 local Entity = {}
 
-function Entity.new(assetId, tweenDuration, killRadius, canEntityKill)
-    local object = game:GetObjects("rbxassetid://" .. assetId)[1]
+function Entity.setup(params)
+    Entity.model = params.Model
+    Entity.speed = params.Speed
+    Entity.moveDelay = params.MoveDelay
+    Entity.heightOffset = params.HeightOffset
+    Entity.canKill = params.CanKill
+    Entity.killRange = params.KillRange
+end
+
+function Entity.create()
+    local object = game:GetObjects(Entity.model)[1]
     local part = object.PrimaryPart
-    local rooms = workspace.CurrentRooms:GetChildren()
     local ts = game:GetService("TweenService")
 
-    local currentRoomIndex = 1
-
-    object.Parent = workspace
-    part.CFrame = rooms[currentRoomIndex].PrimaryPart.CFrame
-
-    local tweenInfo = TweenInfo.new(
-        tweenDuration,
-        Enum.EasingStyle.Linear,
-        Enum.EasingDirection.Out,
-        0,
-        false,
-        0
-    )
-
-    local function createAndPlayTween()
-        local nextRoomIndex = currentRoomIndex % #rooms + 1
-        local nextRoomCFrame = rooms[nextRoomIndex].PrimaryPart.CFrame
-
-        local tween = ts:Create(part, tweenInfo, { CFrame = nextRoomCFrame })
-        tween:Play()
-
-        currentRoomIndex = nextRoomIndex
-
-        if nextRoomIndex == 1 then
-            object:Destroy()  -- Destroy the object when the final room's tween is completed
-        else
-            tween.Completed:Connect(createAndPlayTween)  -- Recursively create and play tween for the next room
-        end
-    end
-
-    createAndPlayTween()
+    local tween = ts:Create(part, TweenInfo.new(1), { CFrame = CFrame.new(10, 0, 0) })
+    tween:Play()
 
     game:GetService("RunService").Stepped:Connect(function()
         for _, player in ipairs(game:GetService("Players"):GetPlayers()) do
-            if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-                local distance = (part.Position - player.Character.HumanoidRootPart.Position).magnitude
-                if distance <= killRadius then
-                    if canEntityKill then
-                        player.Character.Humanoid.Health = 0
-                    else
-                        print("Entity cannot kill.")
-                    end
+            local distance = (part.Position - player.Character.HumanoidRootPart.Position).magnitude
+            if distance <= Entity.killRange then
+                if Entity.canKill then
+                    player.Character.Humanoid.Health = 0
                 end
             end
         end
