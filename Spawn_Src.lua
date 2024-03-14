4ff3132c1d66956bdad078ce8d572d78a074e986
oxiclee <126022978+oxiclee@users.noreply.github.com>
2024-03-13 21:45:38 -0400

    Update Spawn_Src.lua

--- Spawn_Src.lua
+++ Spawn_Src.lua
@@ -35,46 +35,61 @@ function Entity.new(asset, tweenDuration, canEntityKill, delay, backwards, rebou
 
         if not backwards then
             nextRoomIndex = currentRoomIndex + 1
-            if nextRoomIndex > #rooms then
-                if rebounds then 
-                    backwards = true
-                    currentRoomIndex = #rooms - 1
-                else
-                    object:Destroy() 
-                    return
-                end
-            end
         else
             nextRoomIndex = currentRoomIndex - 1
-            if nextRoomIndex < 1 then
-                if rebounds then 
-                    currentReboundCount = currentReboundCount + 1
-                    if currentReboundCount > reboundcount then
-                        object:Destroy()  
-                        return
-                    else        
-                        backwards = false
-                        currentRoomIndex = 2
-                    end
-                else
-                    object:Destroy() 
-                    return
-                end
-            end
         end
         
         local tween = ts:Create(part, tweenInfo, {CFrame = rooms[nextRoomIndex].PrimaryPart.CFrame})
         tween:Play()
 
         tween.Completed:Connect(function()
-            currentRoomIndex = nextRoomIndex
-            createAndPlayTween()
+            if not backwards then
+                if nextRoomIndex >= #rooms then
+                    if rebounds then 
+                        backwards = not backwards
+                        if not backwards then
+                            currentRoomIndex = 1
+                        else
+                            currentRoomIndex = #rooms
+                        end
+                        createAndPlayTween()
+                    else
+                        object:Destroy() 
+                    end
+                else
+                    currentRoomIndex = nextRoomIndex
+                    createAndPlayTween()
+                end
+            else
+                if nextRoomIndex <= 1 then
+                    if rebounds then 
+                        currentReboundCount = currentReboundCount + 1
+                        if currentReboundCount > reboundcount then
+                            object:Destroy()  
+                        else        
+                            backwards = not backwards
+                            createAndPlayTween()
+                        end
+                    end
+                else
+                    currentRoomIndex = nextRoomIndex
+                    createAndPlayTween()
+                end
+            end
         end)
     end
 
     task.wait(delay)
 
-    createAndPlayTween()
+    if not backwards then
+        createAndPlayTween()
+    else
+        local backwardsTween = ts:Create(part, tweenInfo, {CFrame = rooms[#rooms].PrimaryPart.CFrame})
+        backwardsTween:Play()
+        backwardsTween.Completed:Connect(function()
+            createAndPlayTween()
+        end)
+    end
 
     part.Touched:Connect(function(otherPart)
         if otherPart.Parent == game:GetService("Players").LocalPlayer.Character then
