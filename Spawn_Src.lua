dce775dae562a842f41b028039c8ad08173b5ea5
oxiclee <126022978+oxiclee@users.noreply.github.com>
2024-03-13 22:00:56 -0400

    Update Spawn_Src.lua

--- Spawn_Src.lua
+++ Spawn_Src.lua
@@ -1,18 +1,17 @@
 local Entity = {}
 
-function Entity.new(asset, tweenDuration, canEntityKill, delay, backwards, rebounds, reboundcount)
+function Entity.new(asset, tweenDuration, canEntityKill, delay, backwards)
     local object = game:GetObjects(asset)[1]
     local part = object.PrimaryPart
     local roomfolder = workspace.CurrentRooms
     local rooms = roomfolder:GetChildren()
     local ts = game:GetService("TweenService")
 
-    local currentReboundCount = 0
     local currentRoomIndex = nil
-    
+
     if backwards then
         currentRoomIndex = #rooms
-        part.CFrame = rooms[#rooms].Door.PrimaryPart.CFrame
+        part.CFrame = rooms[currentRoomIndex].Door.PrimaryPart.CFrame
     else
         currentRoomIndex = 1
         part.CFrame = rooms[currentRoomIndex].PrimaryPart.CFrame
@@ -30,52 +29,30 @@ function Entity.new(asset, tweenDuration, canEntityKill, delay, backwards, rebou
     )
 
     local function createAndPlayTween()
-        rooms = roomfolder:GetChildren()
-        local nextRoomIndex = nil
+        local nextroomindex = nil
 
         if not backwards then
-            nextRoomIndex = currentRoomIndex + 1
+            nextroomindex = currentRoomIndex + 1
         else
-            nextRoomIndex = currentRoomIndex - 1
+            nextroomindex = currentRoomIndex - 1
         end
         
-        local tween = ts:Create(part, tweenInfo, {CFrame = rooms[nextRoomIndex].PrimaryPart.CFrame})
+        local tween = ts:Create(part, tweenInfo, {CFrame = rooms[nextroomindex].Door.PrimaryPart.CFrame})
         tween:Play()
 
         tween.Completed:Connect(function()
             if not backwards then
-                if nextRoomIndex >= #rooms then
-                    if rebounds then 
-                            
-                        backwards = not backwards
-                            
-                        if not backwards then
-                            currentRoomIndex = 1
-                        else
-                            currentRoomIndex = #rooms
-                        end
-                            
-                        createAndPlayTween()
-                    else
-                        object:Destroy() 
-                    end
+                if nextroomindex >= #rooms-1 then
+                    object:Destroy()
                 else
-                    currentRoomIndex = nextRoomIndex
+                    currentRoomIndex = nextroomindex
                     createAndPlayTween()
                 end
             else
-                if nextRoomIndex <= 1 then
-                    if rebounds then 
-                        currentReboundCount = currentReboundCount + 1
-                        if currentReboundCount > reboundcount then
-                            object:Destroy()  
-                        else        
-                            backwards = not backwards
-                            createAndPlayTween()
-                        end
-                    end
+                if nextroomindex <= 1 then
+                    object:Destroy()
                 else
-                    currentRoomIndex = nextRoomIndex
+                    currentRoomIndex = nextroomindex
                     createAndPlayTween()
                 end
             end
@@ -87,15 +64,15 @@ function Entity.new(asset, tweenDuration, canEntityKill, delay, backwards, rebou
     if not backwards then
         createAndPlayTween()
     else
-        local backwardsTween = ts:Create(part, tweenInfo, {CFrame = rooms[#rooms].PrimaryPart.CFrame})
-        backwardsTween:Play()
-        backwardsTween.Completed:Connect(function()
+        local backwardstween = ts:Create(part, tweenInfo, {CFrame = rooms[#rooms].Door.PrimaryPart.CFrame})
+        backwardstween:Play()
+        backwardstween.Completed:Connect(function()
             createAndPlayTween()
         end)
     end
 
-    part.Touched:Connect(function(otherPart)
-        if otherPart.Parent == game:GetService("Players").LocalPlayer.Character then
+    part.Touched:Connect(function(otherpart)
+        if otherpart.Parent == game:GetService("Players").LocalPlayer.Character then
             if canEntityKill then
                 game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
             else
