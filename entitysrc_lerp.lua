b093f525bebd0658141781cec5dff4f24a056775
oxiclee <126022978+oxiclee@users.noreply.github.com>
2024-03-17 01:02:25 -0400

    Update entitysrc_lerp.lua

--- entitysrc_lerp.lua
+++ entitysrc_lerp.lua
@@ -1,41 +1,30 @@
-local Entity = {}
+local module = {}
 
-function Entity.new(obj, speedFactor, reverses, delay)
-    local object = game:GetObjects(obj)[1]
-    object.Parent = workspace
-    local p = object.PrimaryPart
+function module.new(obj, speedFactor)
+    local obj2 = game:GetObjects(obj)[1]
+    obj2.Parent = workspace
+    local p = obj2.PrimaryPart
 
     local nodes = {}
 
     for _, v in ipairs(workspace:GetDescendants()) do
         if v.Parent.Name == "PathfindNodes" and v.Parent:IsA("Folder") then
-            table.insert(nodes, v.CFrame)
+            table.insert(nodes, v)
         end
     end
 
     local function distanceBetweenCFrames(cframe1, cframe2)
         if cframe1 and cframe2 then
-            return(cframe1.Position - cframe2.Position).magnitude
+            return (cframe1.Position - cframe2.Position).magnitude
         else
             return 0
+        end
     end
 
-    local startNodeIndex, endNodeIndex, step
-    if reverses then
-        startNodeIndex = #nodes
-        endNodeIndex = 1
-        step = -1
-    else
-        startNodeIndex = 1
-        endNodeIndex = #nodes
-        step = 1
-    end
 
-    task.wait(delay)
-    
-    for i = startNodeIndex, endNodeIndex, step do
-        local nodeStart = nodes[i]
-        local nodeEnd = nodes[i + step]
+    for i = 1, #nodes - 1 do
+        local nodeStart = nodes[i].CFrame
+        local nodeEnd = nodes[i + 1].CFrame
         local distance = distanceBetweenCFrames(nodeStart, nodeEnd)
         local startTime = tick()
 
@@ -47,7 +36,8 @@ function Entity.new(obj, speedFactor, reverses, delay)
         end
     end
 
-    object:Destroy()
+    p.CFrame = nodes[#nodes].CFrame
+    obj2:Destroy()
 end
 
-return Entity
+return module
